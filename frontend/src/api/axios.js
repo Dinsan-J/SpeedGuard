58e8fd7521f487215b378d64e5febf83042eb389
ThivaaVipul <144410600+Dinsan-J@users.noreply.github.com>
2025-07-25 21:40:30 +0530

    Update axios baseURL to include protocol for production

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "speed-guard.vercel.app", // backend url
+  baseURL: "https://speed-guard.vercel.app/", // backend url
 });
 
 instance.interceptors.request.use((config) => {
